6309aa7bb559f67098d96fd235c2ccea63121112
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2021-07-26 08:17:43 +0200

    fist commit

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+#![feature(option_result_unwrap_unchecked)]
+
+mod binary_heap;
+pub use binary_heap::BinaryHeap;
+
+mod kary_heap;
+pub use kary_heap::KAryHeap;
+
+mod generic_priority_queue;
+pub use generic_priority_queue::*;
+
+mod priority_queue;
+pub use priority_queue::*;
+
+mod dijkstra_queue;
+pub use dijkstra_queue::*;
